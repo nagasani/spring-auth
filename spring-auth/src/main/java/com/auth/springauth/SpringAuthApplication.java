6abdb355e6e22bbc8b5310b572d57fb84c8e3a99
nagasani <nagasani.r@gmail.com>
2024-05-07 11:00:04 -0400

    Auth

--- spring-auth/src/main/java/com/auth/springauth/SpringAuthApplication.java
+++ spring-auth/src/main/java/com/auth/springauth/SpringAuthApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+@SpringBootApplication	
 @PropertySource("file:${user.dir}/.env")
-public class SpringAuthApplication {
-
+public class SpringAuthApplication 
+{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAuthApplication.class, args);
 	}
